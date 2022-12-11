1342bb32844b0ae046f87eff49dccc54cd4346d6
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-12-10 20:42:56 -0500

    Add files via upload

---
+++ War_Room_Scripts/Uninstall_HP_Client_Security_Manager.ps1
@@ -0,0 +1,23 @@
+﻿Function Uninstall_HP_Client_Security_Manager {
+    $HP_Client_Security_Manager = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%HP Client Security Manager% WHERE NOT '%HP...''" #Add to this
+
+    $uninstall = $HP_Client_Security_Manager.Uninstall()
+
+    Write-Host 'Uninstalling HP Client Security Manager:
+     ' -NoNewline
+
+	$ExitCode = (Start-Process $uninstall).ExitCode
+    
+    if ($ExitCode -eq 0) {
+        Write-Host 'Success! Please restart the computer in order for changes to take effect.' -ForegroundColor Green
+    } else {
+        Write-Host "Failed. There was a problem." -ForegroundColor Red
+        Clean-Up
+        if ($RunScriptSilent -NE $True){
+            Read-Host 'Press [Enter] to exit'
+        }
+	    exit
+    }
+}
+
+Uninstall_HP_Client_Security_Manager
---
+++ War_Room_Scripts/Uninstall_HP_Sure_Click.ps1
@@ -0,0 +1,23 @@
+﻿Function Uninstall_HP_Sure_Click {
+    $HP_Sure_Click = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE '%HP Sure Click% WHERE NOT '%HP...''" #Add to this
+
+    $uninstall = $HP_Sure_Click.Uninstall()
+
+    Write-Host 'Uninstalling HP Sure Click:
+     ' -NoNewline
+
+	$ExitCode = (Start-Process $uninstall).ExitCode
+    
+    if ($ExitCode -eq 0) {
+        Write-Host 'success!' -ForegroundColor Green
+    } else {
+        Write-Host "failed. There was a problem." -ForegroundColor Red
+        Clean-Up
+        if ($RunScriptSilent -NE $True){
+            Read-Host 'Press [Enter] to exit'
+        }
+	    exit
+    }
+}
+
+Uninstall_HP_Sure_Click

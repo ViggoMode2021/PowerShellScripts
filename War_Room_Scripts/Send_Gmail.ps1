a052655f1193eaf315e2a3ce77623155ffd627be
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-12-10 22:23:15 -0500

    Update Send_Gmail.ps1

--- War_Room_Scripts/Send_Gmail.ps1
+++ War_Room_Scripts/Send_Gmail.ps1
@@ -1,2 +1,4 @@
-﻿$mycredentials = Get-Credential
-Send-MailMessage -SmtpServer smtp.gmail.com -Port 587 -UseSsl -From "ryansviglione@gmail.com" -To "ryansviglione@gmail.com" -Subject 'Test subject' -Body 'Test message' -Credential $mycredentials
+$Date = Get-Date
+
+$mycredentials = Get-Credential
+Send-MailMessage -SmtpServer smtp.gmail.com -Port 587 -UseSsl -From "ryansviglione@gmail.com" -To "ryansviglione@gmail.com" -Subject 'Test subject' -Body '$Date' -Attachment "C:\Users\ryans\Desktop\de.jpg" -Credential $mycredentials

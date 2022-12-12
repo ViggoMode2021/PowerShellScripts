345150dd6a818ef15b0ce7a321278dca6069a174
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-12-11 20:29:42 -0500

    Add files via upload

---
+++ War_Room_Scripts/Get_IP.ps1
@@ -0,0 +1,3 @@
+﻿$ip = (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
+
+-join ($ip.split(".") | foreach-object {[system.convert]::tostring($_, 2).padleft(8, "0")})

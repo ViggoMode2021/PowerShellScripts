fda0d3065306b7f17d71351242375b36bec40be6
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-12-15 15:58:16 -0500

    Add files via upload

---
+++ War_Room_Scripts/Westbrook CT Schools Website HTML.ps1
@@ -0,0 +1,52 @@
+﻿$User = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+if (-not $User.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) {
+	Write-Host 'Please run again with Administrator privileges.' -ForegroundColor Red
+    if ($RunScriptSilent -NE $True){
+        Read-Host 'Press [Enter] to exit'
+    }
+    exit
+}
+
+function get_html{
+
+$Item_Path = "C:/Users/ryans/Desktop/html_webpages"
+
+New-Item -Path $Item_Path -ItemType directory
+
+wget https://www.westbrookctschools.org/ -OutFile main_page.html
+
+Write-Host 'Success for main_page.html!' -ForegroundColor Green
+
+wget https://www.westbrookctschools.org/directory/faculty -OutFile faculty.html
+
+Write-Host 'Success for faculty.html!' -ForegroundColor Green
+
+wget https://www.westbrookctschools.org/groups/5743 -OutFile athletics.html
+
+Write-Host 'Success for athletics.html!' -ForegroundColor Green
+
+wget https://www.westbrookctschools.org/groups/5661 -OutFile business_office.html
+
+Write-Host 'Success for business_office.html!' -ForegroundColor Green
+
+wget https://www.westbrookctschools.org/groups/5663 -OutFile curriculum_and_instruction.html
+
+Write-Host 'Success for curriculum_and_instruction.html!' -ForegroundColor Green
+
+wget https://www.westbrookctschools.org/groups/5756 -OutFile food_services.html
+
+Write-Host 'Success for food_services.html!' -ForegroundColor Green
+
+wget https://www.westbrookctschools.org/groups/73867 -OutFile health.html
+
+Write-Host 'Success for health.html!' -ForegroundColor Green
+
+wget https://www.westbrookctschools.org/groups/5754 -OutFile human_resources.html
+
+Write-Host 'Success for human_resources.html!' -ForegroundColor Green
+
+Move-Item -Path powershell_yes.txt -Destination .\desktop\html_webpages
+
+}
+
+get_html 

a6e5ed715ed1e5a42a37ad17dff88b472558d934
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-12-14 22:09:56 -0500

    Add files via upload

---
+++ War_Room_Scripts/Westbrook_High_School_Website.ps1
@@ -0,0 +1,18 @@
+﻿#Start-Process Brave https://www.westbrookctschools.org/
+
+# https://github.com/kamome283/AngleParse
+
+#https://linuxhint.com/run-wget-powershell/
+
+iwr "https://www.westbrookctschools.org/" | Select-HtmlContent "div.html-wrapper-inner", @{
+    Title = "div.html-wrapper-inner > p"
+    Author = "span.link-list-text > p"
+} | select -first 2 | Format-List
+   
+
+$Response = Invoke-WebRequest -URI https://www.westbrookctschools.org/ -UseBasicParsing
+$Response.InputFields | Where-Object {
+    $_.name -like "* Value*"
+} | Select-Object Name, Value
+
+wget https://www.westbrookctschools.org/ -OutFile main_page.html

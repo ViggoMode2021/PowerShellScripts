9bee667bbc6cef754249caabb1a606416646ad19
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2022-12-22 15:50:14 -0500

    Update Automate-Announcements.ps1

--- Automate-Announcements.ps1
+++ Automate-Announcements.ps1
@@ -1,32 +1,44 @@
-function start_and_stop_chrome{
+$videoId = 'WUQoh_1pFzE'
+
+$channelId = 'UCWNxa-_nWe_w9SFfgFF1LoQ'
+
+$GoogleApiKey = 'AIzaSyAuVuGWqJY6506FG7_Gb203nG36d9zrP14'
+
+$video_analytics = 'C:\Users\rviglione\Desktop\Announcements.csv' 
+
+function start_and_stop_announcements{
 Stop-Process -Name msedge
-Start-Process msedge https://www.youtube.com/embed/WUQoh_1pFzE?autoplay=1 #Replace with LiveStream link ttps://www.youtube.com/
+Start-Process msedge https://www.youtube.com/embed/WUQoh_1pFzE?autoplay=1 #Replace with LiveStream link https://www.youtube.com/
 Sleep 5
 Stop-Process -Name msedge
 Start-Process -FilePath  msedge -ArgumentList '--start-fullscreen https://docs.google.com/presentation/d/e/2PACX-1vSmux0MzvHL6vpWuTzi_mvJ4AHdjP7TkKLAiA-nYTt82Wh2VNvECzNq2jCGPRUUoOo2XWFrb2dYypJp/pub?start=true"&"loop=true"&"delayms=10000"&"slide=id.p'
 }
 
-start_and_stop_chrome
+start_and_stop_announcements
 
-$videoId = 'WUQoh_1pFzE'
-
-$GoogleApiKey = 'AIzaSyAuVuGWqJY6506FG7_Gb203nG36d9zrP14'
-
-
+function get_video_date{
 try {
-   (Invoke-RestMethod -uri "https://www.googleapis.com/youtube/v3/videos?id=$videoId&key=$GoogleApiKey&part=snippet,contentDetails,statistics,status").items.statistics.viewCount
-   (Invoke-RestMethod -uri "https://www.googleapis.com/youtube/v3/videos?id=$videoId&key=$GoogleApiKey&part=snippet,contentDetails,statistics,status").items.statistics.likeCount
-   (Invoke-RestMethod -uri "https://www.googleapis.com/youtube/v3/videos?id=$videoId&key=$GoogleApiKey&part=snippet,contentDetails,statistics,status").items.statistics.commentCount
+   
    $Views = (Invoke-RestMethod -uri "https://www.googleapis.com/youtube/v3/videos?id=$videoId&key=$GoogleApiKey&part=snippet,contentDetails,statistics,status").items.statistics.viewCount
    $Likes = (Invoke-RestMethod -uri "https://www.googleapis.com/youtube/v3/videos?id=$videoId&key=$GoogleApiKey&part=snippet,contentDetails,statistics,status").items.statistics.likeCount
    $Comments = (Invoke-RestMethod -uri "https://www.googleapis.com/youtube/v3/videos?id=$videoId&key=$GoogleApiKey&part=snippet,contentDetails,statistics,status").items.statistics.commentCount
 
    $Date = Date
 
-   "This video has $Views views, $Likes likes, and $Comments comments" | Out-File -FilePath C:\Users\ryans\Desktop\$(Get-Date -Format 'ddMMyyyy').txt
+   $NewRow = "$Views,$Likes,$Comments"
+
+   $NewRow | Add-Content -Path $video_analytics 
+
+   $newRow = New-Object PsObject -Property @{ Views = "$Views" ; Likes = $Likes ; Comments = $Comments ; Date = $Date }
+   $fileContent += $newRow
+
+   $filecontent | Export-csv -append $video_analytics
 
 }
 
 catch {
-    "The video's information could not be loaded."
+    Write-Host "The video's information could not be loaded."
 }
+}
+
+get_video_date

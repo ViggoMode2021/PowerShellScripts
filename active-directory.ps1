b05f0d73164175ec9cce9b5b9f6faa4e8b4d786b
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-24 15:49:23 -0500

    Update active-directory.ps1

--- active-directory.ps1
+++ active-directory.ps1
@@ -1,10 +1,18 @@
 Import-Module ActiveDirectory
 
+$Enrollment_Date =  Get-Date -Format "MMddyy"
+
+$Town = "town"
+
+$Password = "$Town$Enrollment_Date"
+
+$Default_Password = $Password | ConvertTo-SecureString -AsPlainText -Force
+
 $First_Name = Read-Host "Enter First Name"
 
 if($First_Name -match "\d+")
 {
-   $First_Name = Read-Host "First name should not contain a number. If you are adament, disregard this."
+   $First_Name = Read-Host "Input can't contain an integer. If you're adament, disregard and try again."
 }
 
 $First_Initial = $First_Name.Substring(0,1).ToLower()
@@ -12,14 +20,14 @@ $Last_Name = Read-Host "Enter Last Name"
 
 if($Last_Name -match "\d+")
 {
-   $Last_Name = Read-Host "Last name should not contain a number. If you are adament, disregard this."
+   $Last_Name = Read-Host "Input can't contain an integer. If you're adament, disregard and try again."
 }
 
 $Last_Name_Lower = $Last_Name.ToLower()
 $Username = "$First_Initial$Last_Name_Lower"
 $Description = Read-Host "Enter a user description."
-$Password = Read-Host -AsSecureString "Enter a secure password" # 
-$Domain = "@.org"
+
+$Domain = "@domain"
 $Comma = ","
 
  New-ADUser `
@@ -30,11 +38,13 @@ $Comma = ","
  -UserPrincipalName "$Username $Domain" `
  -Displayname "$First_Name $Last_Name" `
  -Description $Description `
- -Path "OU=,DC=,DC=" `
- -AccountPassword $Password `
- -ScriptPath ".bat" `
- -HomeDrive ":" `
- -HomeDirectory "" `
+ -Path "OU=,DC=,DC=org" `
+ -AccountPassword $Default_Password `
+ -ScriptPath "logon.bat" `
+ -HomeDrive "Y:" `
+ -HomeDirectory "homedirectory" `
  -EmailAddress "$Username$Domain"
 
 Write-Host "Active Directory user account setup complete for $First_Name $Last_Name!"
+
+Enable-ADAccount -Identity $Username 

27913b117f19ff58928d878a23242e6db142d8a8
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-03-17 23:13:56 -0400

    Update SetUp-AD.ps1

--- SetUp-AD.ps1
+++ SetUp-AD.ps1
@@ -14,6 +14,24 @@ $MiddleSchool = "MiddleSchool"
 
 $ElementarySchool = "ElementarySchool"
 
+$OrganizationalUnits = @("StudentAccounts", "HighSchool", "Class of 2023", "Class of 2024", "Class of 2025", "Class of 2026", "Class of 2027", 
+"MiddleSchool", "Class of 2027", "Class of 2028", "Class of 2029", "Class of 2030", "ElementarySchool", "Class of 2031", "Class of 2032", "Class of 2033", "Class of 2034")
+
+function CheckOU{
+    foreach ($OU in $OrganizationalUnits){
+        if (Get-ADOrganizationalUnit -Filter 'Name -like $OU' | Format-Table Name, DistinguishedName -A) {
+            Write-Host "$OU already exists."
+}
+else {
+
+Write-Host "Creating OU named $OU"
+}
+    
+}
+}
+
+CheckOU
+
 New-ADOrganizationalUnit -Name "StudentAccounts" -Path "DC=$DC,DC=COM"
 
 New-ADOrganizationalUnit -Name "HighSchool" -Path "OU=$StudentAccounts,DC=$DC,DC=COM"
@@ -45,21 +63,3 @@ New-ADOrganizationalUnit -Name "Class of 2032" -Path "OU=$ElementarySchool,OU=$S
 New-ADOrganizationalUnit -Name "Class of 2033" -Path "OU=$ElementarySchool,OU=$StudentAccounts,DC=$DC,DC=COM"
 
 New-ADOrganizationalUnit -Name "Class of 2034" -Path "OU=$ElementarySchool,OU=$StudentAccounts,DC=$DC,DC=COM"
-
-$OrganizationalUnits = @("StudentAccounts", "HighSchool", "Class of 2023", "Class of 2024", "Class of 2025", "Class of 2026", "Class of 2027", 
-"MiddleSchool", "Class of 2027", "Class of 2028", "Class of 2029", "Class of 2030", "ElementarySchool", "Class of 2031", "Class of 2032", "Class of 2033", "Class of 2034")
-
-function CheckOU{
-    foreach ($OU in $OrganizationalUnits){
-        if (Get-ADOrganizationalUnit -Filter 'Name -like $OU' | Format-Table Name, DistinguishedName -A) {
-            Write-Host "$OU already exists."
-}
-else {
-
-Write-Host "Creating OU named $OU"
-}
-    
-}
-}
-
-CheckOU

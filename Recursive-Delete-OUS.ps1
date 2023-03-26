f232bdcd4e552acd1379d766a8c7d90702cfb350
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-03-26 07:42:01 -0700

    Update Recursive-Delete-OUS.ps1

--- Recursive-Delete-OUS.ps1
+++ Recursive-Delete-OUS.ps1
@@ -1,3 +1,9 @@
-Get-ADOrganizationalUnit -Identity 'OU=,DC=,DC=COM' |
+Get-ADOrganizationalUnit -Identity 'OU=FacultyAccounts,DC=vigschools,DC=ORG' |
     Set-ADObject -ProtectedFromAccidentalDeletion:$false -PassThru |
     Remove-ADOrganizationalUnit -Recursive -Confirm:$false
+
+Get-ADOrganizationalUnit -Identity 'OU=StudentAccounts,DC=vigschools,DC=ORG' |
+    Set-ADObject -ProtectedFromAccidentalDeletion:$false -PassThru |
+    Remove-ADOrganizationalUnit -Recursive -Confirm:$false
+
+Remove-Item C:\Users\Administrator\Desktop\CSV-Data 

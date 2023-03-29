17006a12a594f96a44317757b2b1fd0a9467bfe6
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-03-29 15:16:46 -0700

    Create Get-GPO-Reports.ps1

---
+++ Get-GPO-Reports.ps1
@@ -0,0 +1,3 @@
+Get-GPOReport -Name 'Faculty-Students-GPO' -ReportType 'HTML' -Path 'C:\Users\Administrator\Desktop\GPO_Reports\Faculty-Students-GPO-Report'
+
+Get-GPOReport -Name 'Faculty-Students-GPO' -ReportType 'HTML' -Path 'C:\Users\Administrator\Desktop\GPO_Reports\Faculty-Students-GPO-Report-XML'

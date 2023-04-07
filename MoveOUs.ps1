3946b998e9a44e972c0fc5b02fbdb2a64667e0d6
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-07 09:08:46 -0700

    Update MoveOUs.ps1

--- MoveOUs.ps1
+++ MoveOUs.ps1
@@ -29,8 +29,11 @@ Write-Host "It is too early to move the Organizational Units for the new school 
 
 else{
 
+# Remove -ProtectedFromAccidentalDeletion $Flase in New OU below when running script in prod. 
+
 Try {
   New-ADOrganizationalUnit -Name "ClassOf$New_Elementary_School_Graduation_Year" -Path "OU=ElementarySchool,OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
+  Write-Host "Creating a new OU titled ClassOf$New_Elementary_School_Graduation_Year."
 }
 Catch {
   Write-Host "A ClassOf$New_Elementary_School_Graduation_Year OU already exists."
@@ -38,16 +41,30 @@ Catch {
 
 $Graduating_Elementary_OU = $Elementary_School_OUs | select -first 1
 
+Write-Host "Moving $Graduating_Elementary_OU to MiddleSchool OU."
+
+Move-ADObject $Graduating_Elementary_OU -TargetPath "OU=StudentAccounts,OU=MiddleSchool,DC=vigschools,DC=org"
+
 $Graduating_Middle_OU = $Middle_School_OUs | select -first 1
+
+Write-Host "Moving $Graduating_Middle_OU to HighSchool OU."
+
+Move-ADObject $Graduating_Middle_OU -TargetPath "OU=StudentAccounts,OU=HighSchool,DC=vigschools,DC=org"
 
 $Graduating_High_OU = $High_School_OUs | select -first 1
 
+# Remove -ProtectedFromAccidentalDeletion $Flase in New OU below when running script in prod. 
 
 Try {
   New-ADOrganizationalUnit -Name "Graduated_Classes" -Path "OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
+  Write-Host "Creating a new OU titled Graduated_Classes."
+  Write-Host "Moving $Graduating_High_OU to HighSchool OU."
+  Move-ADObject $Graduating_High_OU -TargetPath "OU=StudentAccounts,OU=Graduated_Classes,DC=vigschools,DC=org"
 }
 Catch {
   Write-Host "A Graduated_Classes OU already exists."
+  Write-Host "Moving $Graduating_High_OU to HighSchool OU."
+  Move-ADObject $Graduating_High_OU -TargetPath "OU=StudentAccounts,OU=Graduated_Classes,DC=vigschools,DC=org"
 }
 
 }

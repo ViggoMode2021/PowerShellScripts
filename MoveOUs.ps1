784db4571864aa6f05a5f3c4b8698b8d1bd8afcf
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-07 08:14:16 -0700

    Update MoveOUs.ps1

--- MoveOUs.ps1
+++ MoveOUs.ps1
@@ -4,26 +4,33 @@ $OU_Move_Date = [DateTime]"07/01/$Current_Year"
 
 $Today_Full_Date = Get-Date -Format "MM/dd/yyyy"
 
-$OU = 'OU=ElementarySchool,OU=StudentAccounts,DC=vigschools,DC=org'
+$Elementary_School_Top_OU = 'OU=ElementarySchool,OU=StudentAccounts,DC=vigschools,DC=org'
 
-$Elementary_School_OUs = Get-ADOrganizationalUnit -SearchBase $OU -SearchScope Subtree -Filter "name -like '*Class*'" | 
+$Middle_School_Top_OU = 'OU=MiddleSchool,OU=StudentAccounts,DC=vigschools,DC=org'
+
+$High_School_Top_OU = 'OU=HighSchool,OU=StudentAccounts,DC=vigschools,DC=org'
+
+$Elementary_School_OUs = Get-ADOrganizationalUnit -SearchBase $Elementary_School_Top_OU -SearchScope Subtree -Filter "name -like '*Class*'" | 
+     Select-Object Name
+
+$Middle_School_OUs = Get-ADOrganizationalUnit -SearchBase $Middle_School_Top_OU -SearchScope Subtree -Filter "name -like '*Class*'" | 
      Select-Object Name 
 
-if($Today_Full_Date -lt $OU_Move_Date){
+$High_School_OUs = Get-ADOrganizationalUnit -SearchBase $High_School_Top_OU -SearchScope Subtree -Filter "name -like '*Class*'" | 
+     Select-Object Name 
 
-#Write-Host "It is too early to move the Organizational Units for the new school year."
+if($OU_Move_Date -lt $Today_Full_Date){ # << Swap position of these two variables in when executing script in prod 
 
-foreach ($Unit in $Elementary_School_OUs){
-
-$Unit.Name.Substring($Unit.Name.Length - 4)
+Write-Host "It is too early to move the Organizational Units for the new school year."
 
 }
 
-
-} 
-
 else{
 
+$Graduating_Elementary_OU = $Elementary_School_OUs | select -first 1
 
+$Graduating_Middle_OU = $Middle_School_OUs | select -first 1
+
+$Graduating_High_OU = $High_School_OUs | select -first 1
 
 }

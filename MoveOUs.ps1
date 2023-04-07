23325cacdc126b9920ab279a3296ab66789b7498
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-07 08:49:11 -0700

    Update MoveOUs.ps1

--- MoveOUs.ps1
+++ MoveOUs.ps1
@@ -1,5 +1,7 @@
 $Current_Year = Get-Date -Format "yyyy"
 
+$New_Elementary_School_Graduation_Year = [int]$Current_Year + 12
+
 $OU_Move_Date = [DateTime]"07/01/$Current_Year"
 
 $Today_Full_Date = Get-Date -Format "MM/dd/yyyy"
@@ -27,10 +29,25 @@ Write-Host "It is too early to move the Organizational Units for the new school 
 
 else{
 
+Try {
+  New-ADOrganizationalUnit -Name "ClassOf$New_Elementary_School_Graduation_Year" -Path "OU=ElementarySchool,OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
+}
+Catch {
+  Write-Host "A ClassOf$New_Elementary_School_Graduation_Year OU already exists."
+}
+
 $Graduating_Elementary_OU = $Elementary_School_OUs | select -first 1
 
 $Graduating_Middle_OU = $Middle_School_OUs | select -first 1
 
 $Graduating_High_OU = $High_School_OUs | select -first 1
+
+
+Try {
+  New-ADOrganizationalUnit -Name "Graduated_Classes" -Path "OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
+}
+Catch {
+  Write-Host "A Graduated_Classes OU already exists."
+}
 
 }

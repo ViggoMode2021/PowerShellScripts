56fe91667a2014623e717bf2a470ed52abb1380e
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-07 07:03:50 -0700

    Update MoveOUs.ps1

--- MoveOUs.ps1
+++ MoveOUs.ps1
@@ -3,3 +3,14 @@ $Current_Year = Get-Date -Format "yyyy"
 $OU_Move_Date = [DateTime]"07/01/$Current_Year"
 
 $Today_Full_Date = Get-Date -Format "MM/dd/yyyy"
+
+if($Today_Full_Date -lt $OU_Move_Date){
+
+Write-Host "It is too early to move the Organizational Units for the new school year."
+} 
+
+else{
+
+
+
+}

2c25b69ef53d4f0552d0a43b0fd048968b9506b3
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-07 10:12:27 -0700

    Update MoveOUs.ps1

--- MoveOUs.ps1
+++ MoveOUs.ps1
@@ -29,31 +29,23 @@ Write-Host "It is too early to move the Organizational Units for the new school 
 
 else{
 
-# Remove -ProtectedFromAccidentalDeletion $Flase in New OU below when running script in prod. 
-
-Try {
-  New-ADOrganizationalUnit -Name "ClassOf$New_Elementary_School_Graduation_Year" -Path "OU=ElementarySchool,OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
-  Write-Host "Creating a new OU titled ClassOf$New_Elementary_School_Graduation_Year."
-}
-Catch {
-  Write-Host "A ClassOf$New_Elementary_School_Graduation_Year OU already exists."
-}
-
 $Graduating_Elementary_OU = $Elementary_School_OUs | select -first 1
+
+$Graduating_Elementary_OU
 
 Write-Host "Moving $Graduating_Elementary_OU to MiddleSchool OU."
 
-$Graduating_Elementary_OU=Get-ADOrganizationalUnit -Filter 'Name -like "$Graduating_Elementary_OU"'
+$Graduating_Elementary_OU_Name = Get-ADOrganizationalUnit -Identity "OU=$Graduating_Elementary_OU,OU=ElementarySchool,OU=StudentAccounts,OU=MiddleSchool,DC=vigschools,DC=org"
 
-echo $Graduating_Elementary_OU
-
-Move-ADObject $Graduating_Elementary_OU -TargetPath "OU=StudentAccounts,OU=MiddleSchool,DC=vigschools,DC=org"
+Move-ADObject $Graduating_Elementary_OU_Name -TargetPath "OU=MiddleSchool,OU=StudentAccounts,DC=vigschools,DC=org"
 
 $Graduating_Middle_OU = $Middle_School_OUs | select -first 1
 
 Write-Host "Moving $Graduating_Middle_OU to HighSchool OU."
 
-Move-ADObject $Graduating_Middle_OU -TargetPath "OU=StudentAccounts,OU=HighSchool,DC=vigschools,DC=org"
+$Graduating_Middle_OU = Get-ADOrganizationalUnit -Identity "OU=$Graduating_Middle_OU,OU=MiddleSchool,OU=StudentAccounts,OU=MiddleSchool,DC=vigschools,DC=org"
+
+Move-ADObject $Graduating_Middle_OU -TargetPath "OU=HighSchool,OU=StudentAccounts,DC=vigschools,DC=org"
 
 $Graduating_High_OU = $High_School_OUs | select -first 1
 
@@ -63,14 +55,24 @@ Try {
   New-ADOrganizationalUnit -Name "Graduated_Classes" -Path "OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
   Write-Host "Creating a new OU titled Graduated_Classes."
   Write-Host "Moving $Graduating_High_OU to HighSchool OU."
-  $Graduating_High_OU = Get-ADOrganizationalUnit -Filter 'Name -like "*$Graduating_High_OU*"'
-  Move-ADObject $Graduating_High_OU -TargetPath "OU=StudentAccounts,OU=Graduated_Classes,DC=vigschools,DC=org"
+  $Graduating_High_OU = Get-ADOrganizationalUnit -Identity "OU=$Graduating_High_OU,OU=HighSchool,OU=StudentAccounts,DC=vigschools,DC=org"
+  Move-ADObject $Graduating_High_OU -TargetPath "OU=Graduated_Classes,OU=StudentAccounts,DC=vigschools,DC=org"
 }
 Catch {
   Write-Host "A Graduated_Classes OU already exists."
   Write-Host "Moving $Graduating_High_OU to HighSchool OU."
   $Graduating_High_OU = Get-ADOrganizationalUnit -Filter 'Name -like "*$Graduating_High_OU*"'
-  Move-ADObject $Graduating_High_OU -TargetPath "OU=StudentAccounts,OU=Graduated_Classes,DC=vigschools,DC=org"
+  Move-ADObject $Graduating_High_OU -TargetPath "OU=Graduated_Classes,OU=StudentAccounts,DC=vigschools,DC=org"
+}
+
+# Remove -ProtectedFromAccidentalDeletion $Flase in New OU below when running script in prod. 
+
+Try {
+  New-ADOrganizationalUnit -Name "ClassOf$New_Elementary_School_Graduation_Year" -Path "OU=ElementarySchool,OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
+  Write-Host "Creating a new OU titled ClassOf$New_Elementary_School_Graduation_Year."
+}
+Catch {
+  Write-Host "A ClassOf$New_Elementary_School_Graduation_Year OU already exists."
 }
 
 }

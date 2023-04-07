1e8b2a76d6e756c31058e8094d8c02f43207e408
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-07 09:41:04 -0700

    Update MoveOUs.ps1

--- MoveOUs.ps1
+++ MoveOUs.ps1
@@ -13,13 +13,13 @@ $Middle_School_Top_OU = 'OU=MiddleSchool,OU=StudentAccounts,DC=vigschools,DC=org
 $High_School_Top_OU = 'OU=HighSchool,OU=StudentAccounts,DC=vigschools,DC=org'
 
 $Elementary_School_OUs = Get-ADOrganizationalUnit -SearchBase $Elementary_School_Top_OU -SearchScope Subtree -Filter "name -like '*Class*'" | 
-     Select-Object Name
+     Select-Object Name -ExpandProperty Name
 
 $Middle_School_OUs = Get-ADOrganizationalUnit -SearchBase $Middle_School_Top_OU -SearchScope Subtree -Filter "name -like '*Class*'" | 
-     Select-Object Name 
+     Select-Object Name -ExpandProperty Name
 
 $High_School_OUs = Get-ADOrganizationalUnit -SearchBase $High_School_Top_OU -SearchScope Subtree -Filter "name -like '*Class*'" | 
-     Select-Object Name 
+     Select-Object Name -ExpandProperty Name
 
 if($OU_Move_Date -lt $Today_Full_Date){ # << Swap position of these two variables in when executing script in prod 
 
@@ -43,6 +43,10 @@ $Graduating_Elementary_OU = $Elementary_School_OUs | select -first 1
 
 Write-Host "Moving $Graduating_Elementary_OU to MiddleSchool OU."
 
+$Graduating_Elementary_OU=Get-ADOrganizationalUnit -Filter 'Name -like "$Graduating_Elementary_OU"'
+
+echo $Graduating_Elementary_OU
+
 Move-ADObject $Graduating_Elementary_OU -TargetPath "OU=StudentAccounts,OU=MiddleSchool,DC=vigschools,DC=org"
 
 $Graduating_Middle_OU = $Middle_School_OUs | select -first 1
@@ -59,11 +63,13 @@ Try {
   New-ADOrganizationalUnit -Name "Graduated_Classes" -Path "OU=StudentAccounts,DC=vigschools,DC=org" -ProtectedFromAccidentalDeletion $False 
   Write-Host "Creating a new OU titled Graduated_Classes."
   Write-Host "Moving $Graduating_High_OU to HighSchool OU."
+  $Graduating_High_OU = Get-ADOrganizationalUnit -Filter 'Name -like "*$Graduating_High_OU*"'
   Move-ADObject $Graduating_High_OU -TargetPath "OU=StudentAccounts,OU=Graduated_Classes,DC=vigschools,DC=org"
 }
 Catch {
   Write-Host "A Graduated_Classes OU already exists."
   Write-Host "Moving $Graduating_High_OU to HighSchool OU."
+  $Graduating_High_OU = Get-ADOrganizationalUnit -Filter 'Name -like "*$Graduating_High_OU*"'
   Move-ADObject $Graduating_High_OU -TargetPath "OU=StudentAccounts,OU=Graduated_Classes,DC=vigschools,DC=org"
 }
 

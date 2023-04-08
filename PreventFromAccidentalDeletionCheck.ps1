e2ee483f25ff9fb6967ade8b66b9adfee76fd152
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-08 07:39:44 -0400

    Add files via upload

---
+++ PreventFromAccidentalDeletionCheck.ps1
@@ -0,0 +1,5 @@
+﻿Get-ADOrganizationalUnit -filter * -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | ft
+
+Get-ADOrganizationalUnit -filter * -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $true
+
+Get-ADOrganizationalUnit -filter * -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion} | ft

dabfcafd3643c4408c6febdfc7edcd4e31e98b63
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-04-18 18:50:47 -0400

    Create AD-Password-Generator.ps1

--- AD-Password-Generator.ps1
+++ AD-Password-Generator.ps1
@@ -29,6 +29,11 @@ $Combo_Box_Object = [System.Windows.Forms.ComboBox] # Dropdown object
 
 $Application_Form = New-Object $Form_Object # Create new form/window for GUI
 
+
+$Application_Form.Controls.Add($groupbox1)
+$Application_Form.Controls.Add($groupbox2)
+
+
 $Application_Form.ClientSize= '500,300'
 
 #$Domain = Get-ADDomain -Current LocalComputer | Select Name | foreach { $_.Name } |  Out-String # Get-AD Domain name
@@ -51,7 +56,7 @@ $Password_Length.AutoSize = $true
 
 $Password_Length.Font = 'Verdana,8,style=Bold'
 
-$Password_Length.Location = New-Object System.Drawing.Point(220,20)
+$Password_Length.Location = New-Object System.Drawing.Point(200,20)
 
 ## ---------------------------------------------------------------------------- ## 
 
@@ -70,6 +75,11 @@ $Password_Theme.Location = New-Object System.Drawing.Point(420,20)
 ## ---------------------------------------------------------------------------- ## 
 
 # Password length choice radio buttons:
+
+$groupbox1 = New-Object System.Windows.Forms.GroupBox
+
+$groupbox1.Location = '10,10'
+$groupbox1.size = '420,400'
 
 $Password_Length_Option_1 = New-Object System.Windows.Forms.RadioButton
 $Password_Length_Option_2 = New-Object System.Windows.Forms.RadioButton
@@ -103,9 +113,23 @@ $Password_Length_Option_6.Name = "10 characters maximum"
 $Password_Length_Option_6.Text = "10"
 $Password_Length_Option_6.Location = New-Object System.Drawing.Point(320,220)
 
+$groupbox1.Controls.Add($Password_Length_Option_1)
+$groupbox1.Controls.Add($Password_Length_Option_2)
+$groupbox1.Controls.Add($Password_Length_Option_3)
+$groupbox1.Controls.Add($Password_Length_Option_4)
+$groupbox1.Controls.Add($Password_Length_Option_5)
+$groupbox1.Controls.Add($Password_Length_Option_6)
+
+$groupbox1.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
+
 ## ---------------------------------------------------------------------------- ## 
 
 # Password theme option radio buttons:
+
+$groupbox2 = New-Object System.Windows.Forms.GroupBox
+
+$groupbox2.Location = '500,50'
+$groupbox2.size = '220,200'
 
 $Password_Theme_Option_1 = New-Object System.Windows.Forms.RadioButton
 $Password_Theme_Option_2 = New-Object System.Windows.Forms.RadioButton
@@ -134,6 +158,17 @@ $Password_Theme_Option_5.Name = "Random"
 $Password_Theme_Option_5.Text = "Random"
 $Password_Theme_Option_5.Location = New-Object System.Drawing.Point(520,220)
 
+# Add all objects to the form:
+
+$groupbox2.Controls.Add($Password_Theme_Option_1)
+$groupbox2.Controls.Add($Password_Theme_Option_2)
+$groupbox2.Controls.Add($Password_Theme_Option_3)
+$groupbox2.Controls.Add($Password_Theme_Option_4)
+$groupbox2.Controls.Add($Password_Theme_Option_5)
+$groupbox2.Controls.Add($Password_Theme_Option_6)
+
+$groupbox2.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation
+
 ## ---------------------------------------------------------------------------- ##
 
 $Misc_Password_Params = New-Object System.Windows.Forms.CheckedListBox
@@ -142,9 +177,11 @@ $Misc_Password_Params.Location = New-Object System.Drawing.Size(700,50)
 
 $Misc_Password_Params.Size = New-Object System.Drawing.Size(200,180)
 
-$Misc_Password_Params.Items.Insert(0, "Copenhagen"); 
+$Misc_Password_Params.Items.Insert(0, "Capital first letter"); 
 
-$Misc_Password_Params.Items.Insert(1, "Italy"); 
+$Misc_Password_Params.Items.Insert(1, "Include numbers"); 
+
+$Misc_Password_Params.Items.Insert(2, "Include special characters"); 
 
 #$Misc_Password_Params.Items.AddRange(1..5)
 
@@ -156,7 +193,100 @@ $Misc_Password_Params.Size = New-Object System.Drawing.Size(500,300)
 $Misc_Password_Params.Height = 200
 $Misc_Password_Params.Font = New-Object System.Drawing.Font("Lucida Console",12,[System.Drawing.FontStyle]::Regular)
 
-# Add all objects to the form:
+## ---------------------------------------------------------------------------- ##
+
+function Generate_Password{
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+
+if ($Password_Length_Option_1.Checked){
+
+$Password_Length_Selection = 5
+
+}
+
+if ($Password_Length_Option_2.Checked){
+
+$Password_Length_Selection = 6
+
+}
+
+if ($Password_Length_Option_3.Checked){
+
+$Password_Length_Selection = 7
+
+}
+
+if ($Password_Length_Option_4.Checked){
+
+$Password_Length_Selection = 8
+
+}
+
+if ($Password_Length_Option_5.Checked){
+
+$Password_Length_Selection = 9
+
+}
+
+if ($Password_Length_Option_6.Checked){
+
+$Password_Length_Selection = 10
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## ---------------------------------------------------------------------------- ##
+
+$Button_Object = [System.Windows.Forms.Button]
+
+$Create_Password_Button = New-Object $Button_Object
+
+$Create_Password_Button.Text= "Generate Password"
+
+$Create_Password_Button.AutoSize = $true
+
+$Create_Password_Button.Font = 'Verdana,12,style=Bold'
+
+$Create_Password_Button.Location = New-Object System.Drawing.Point(550,350)
+
+$Create_Password_Button.Add_Click({Generate_Password})
+
+$Application_Form.DataBindings.DefaultDataSourceUpdateMode = [System.Windows.Forms.DataSourceUpdateMode]::OnValidation 
+
+$Application_Form.Controls.AddRange(@($Password_Length, $Password_Length_Option_1, $Password_Length_Option_2, $Password_Length_Option_3, 
+$Password_Length_Option_4, $Password_Length_Option_5, $Password_Length_Option_6, $Password_Theme, $Password_Theme_Option_1, $Password_Theme_Option_2, $Password_Theme_Option_3,
+$Password_Theme_Option_4, $Password_Theme_Option_5, $Password_Theme_Option_6, $Misc_Password_Params, $Create_Password_Button, $groupbox1, $groupbox2))
+
+$Application_Form.ShowDialog() # Show form on runtime
+
+$Application_Form.Dispose() # Garbage collection Add all objects to the form:
 
 <#$CheckedListBox#>
 

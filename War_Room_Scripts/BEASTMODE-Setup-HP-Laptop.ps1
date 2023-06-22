a23afbbe9794a75b11f0551e2b76f16aadcf70b0
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-06-22 14:06:12 -0400

    Create BEASTMODE-Setup-HP-Laptop.ps1

---
+++ War_Room_Scripts/BEASTMODE-Setup-HP-Laptop.ps1
@@ -0,0 +1,131 @@
+# WPS HP Laptop setup script #
+
+# Step 1 - Remove HP Bloatware #
+
+# List of built-in apps to remove
+$UninstallPackages = @(
+    "AD2F1837.HPJumpStarts"
+    "AD2F1837.HPPCHardwareDiagnosticsWindows"
+    "AD2F1837.HPPowerManager"
+    "AD2F1837.HPPrivacySettings"
+    "AD2F1837.HPSupportAssistant"
+    "AD2F1837.HPSureShieldAI"
+    "AD2F1837.HPSystemInformation"
+    "AD2F1837.HPQuickDrop"
+    "AD2F1837.HPWorkWell"
+    "AD2F1837.myHP"
+    "AD2F1837.HPDesktopSupportUtilities"
+    "AD2F1837.HPQuickTouch"
+    "AD2F1837.HPEasyClean"
+    "AD2F1837.HPSystemInformation"
+)
+
+# List of programs to uninstall
+$UninstallPrograms = @(
+    "HP Client Security Manager"
+    "HP Connection Optimizer"
+    "HP Documentation"
+    "HP MAC Address Manager"
+    "HP Notifications"
+    "HP Security Update Service"
+    "HP System Default Settings"
+    "HP Sure Click"
+    "HP Sure Click Security Browser"
+    "HP Presence Aware Provider"
+    "HP Sure Run"
+    "HP Sure Recover"
+    "HP Sure Sense"
+    "HP Sure Sense Installer"
+    "HP Wolf Security"
+    "HP Wolf Security Application Support for Sure Sense"
+    "HP Wolf Security Application Support for Windows"
+)
+
+$HPidentifier = "AD2F1837"
+
+$InstalledPackages = Get-AppxPackage -AllUsers `
+            | Where-Object {($UninstallPackages -contains $_.Name) -or ($_.Name -match "^$HPidentifier")}
+
+$ProvisionedPackages = Get-AppxProvisionedPackage -Online `
+            | Where-Object {($UninstallPackages -contains $_.DisplayName) -or ($_.DisplayName -match "^$HPidentifier")}
+
+$InstalledPrograms = Get-Package | Where-Object {$UninstallPrograms -contains $_.Name}
+
+# Remove appx provisioned packages - AppxProvisionedPackage
+ForEach ($ProvPackage in $ProvisionedPackages) {
+
+    Write-Host -Object "Attempting to remove provisioned package: [$($ProvPackage.DisplayName)]..."
+
+    Try {
+        $Null = Remove-AppxProvisionedPackage -PackageName $ProvPackage.PackageName -Online -ErrorAction Stop
+        Write-Host -Object "Successfully removed provisioned package: [$($ProvPackage.DisplayName)]"
+    }
+    Catch {Write-Warning -Message "Failed to remove provisioned package: [$($ProvPackage.DisplayName)]"}
+}
+
+# Remove appx packages - AppxPackage
+ForEach ($AppxPackage in $InstalledPackages) {
+                                            
+    Write-Host -Object "Attempting to remove Appx package: [$($AppxPackage.Name)]..."
+
+    Try {
+        $Null = Remove-AppxPackage -Package $AppxPackage.PackageFullName -AllUsers -ErrorAction Stop
+        Write-Host -Object "Successfully removed Appx package: [$($AppxPackage.Name)]"
+    }
+    Catch {Write-Warning -Message "Failed to remove Appx package: [$($AppxPackage.Name)]"}
+}
+
+# Remove installed programs
+$InstalledPrograms | ForEach-Object {
+
+    Write-Host -Object "Attempting to uninstall: [$($_.Name)]..."
+
+    Try {
+        $Null = $_ | Uninstall-Package -AllVersions -Force -ErrorAction Stop
+        Write-Host -Object "Successfully uninstalled: [$($_.Name)]"
+    }
+    Catch {Write-Warning -Message "Failed to uninstall: [$($_.Name)]"}
+}
+
+# Fallback attempt 1 to remove HP Wolf Security using msiexec
+Try {
+    MsiExec /x "{0E2E04B0-9EDD-11EB-B38C-10604B96B11E}" /qn /norestart
+    Write-Host -Object "Fallback to MSI uninistall for HP Wolf Security initiated"
+}
+Catch {
+    Write-Warning -Object "Failed to uninstall HP Wolf Security using MSI - Error message: $($_.Exception.Message)"
+}
+
+# Fallback attempt 2 to remove HP Wolf Security using msiexec
+Try {
+    MsiExec /x "{4DA839F0-72CF-11EC-B247-3863BB3CB5A8}" /qn /norestart
+    Write-Host -Object "Fallback to MSI uninistall for HP Wolf 2 Security initiated"
+}
+Catch {
+    Write-Warning -Object  "Failed to uninstall HP Wolf Security 2 using MSI - Error message: $($_.Exception.Message)"
+}
+
+
+# Step 2 - Uninstall Microsoft 365, I will keep trying to figure out how to do this via PowerShell # 
+
+# Step 3 - Change local group policy to enable 'Turn off Microsoft consumer experiences' by creating a new key titled CloudContent in HKLM hive #
+
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name CloudContent
+
+Set-Itemproperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent' -Type 'DWord' -Name 'DisableCloudOptimizedContent' -value '1'
+
+# Step 4 - Create DWORD and set value to 0 for 'EnableFirstLogonAnimation'
+
+Set-Itemproperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Type 'DWord' -Name 'EnableFirstLogonAnimation' -value '0'
+
+# Step 5 - Set timezone 
+
+Set-TimeZone -Id "Eastern Standard Time"
+
+# Change computer name and add to domain
+
+$Computer_Name = hostname
+
+$New_Computer_Name = Read-Host "Type the new computer name here."
+
+Add-Computer -DomainName westbrookctschools.org -ComputerName $Computer_Name -newname $New_Computer_Name

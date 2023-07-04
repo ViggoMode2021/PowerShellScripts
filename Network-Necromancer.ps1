b088bc03a02db31ca4078ccd16d01a6418d74830
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-07-04 12:54:17 -0400

    Update Network-Necromancer.ps1

--- Network-Necromancer.ps1
+++ Network-Necromancer.ps1
@@ -17,12 +17,20 @@ in the event that you may have forgotten it. What would you like to do?
 
 Please type the corresponding number here" 
 
+####################################################### First main option ###############################################################
+
 if($Introduction -eq "1"){
 
 $All_Networks = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} |
 %{[PSCustomObject]@{ Network = $name}} | Format-Table -AutoSize | Out-Host
 
 Write-Host $All_Networks 
+
+$Network_Count = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} |
+%{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} |
+%{[PSCustomObject]@{ Network = $name;Password = $pass }} | Measure-Object | Select-Object -expandproperty Count | Out-String
+
+Write-Host "You currently have $Network_Count networks on your device." 
 
 $Introduction_1_Second__Prompt = Read-Host "What would you like to do?
 
@@ -32,11 +40,13 @@ $Introduction_1_Second__Prompt = Read-Host "What would you like to do?
 
 if($Introduction_1_Second__Prompt -eq "3"){
 
-Invoke-Expression Introduction
-}
-
+Introduction
 
 }
+
+}
+
+####################################################### Second main option ###############################################################
 
 if($Introduction -eq "2"){
 
@@ -44,19 +54,35 @@ if($Introduction -eq "2"){
 %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | 
 %{[PSCustomObject]@{ Network = $name;Password = $pass }} | Format-Table -AutoSize 
 
+$Network_Count = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} |
+%{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} |
+%{[PSCustomObject]@{ Network = $name;Password = $pass }} | Measure-Object | Select-Object -expandproperty Count | Out-String
+
+Write-Host "You currently have '$Network_Count' networks on your device."
+
+Sleep 2 
+
+Introduction
+
 }
+
+####################################################### Third main option ###############################################################
 
 if($Introduction -eq "3"){
 
 if (Test-Path "XML-Network-Info"){
 
-$Network_Search = Read-Host "All networks will be exported to the desktop. Press any key to continue."
+$Network_Search = Read-Host "All networks will be exported to the 'XML-Network-Info' directory in the same directory as this script. Press any key to continue."
 
 netsh wlan export profile key=clear folder='XML-Network-Info'
 
 $Total_XML_Files = ( Get-ChildItem XML-Network-Info | Measure-Object ).Count;
 
-Write-Host "$Total_XML_Files network XML files exported to the desktop."
+Write-Host "$Total_XML_Files network XML files exported to the 'XML-Network-Info' directory."
+
+Sleep 5
+
+Introduction
 
 }
 
@@ -64,13 +90,17 @@ else{
 
 New-Item -Path 'XML-Network-Info' -ItemType Directory
 
-Write-Host "Creating a directory titled 'XML-Network-Info' on the desktop."
+Write-Host "All networks will be exported to the 'XML-Network-Info' directory in the same directory as this script. Press any key to continue."
 
 netsh wlan export profile key=clear folder='XML-Network-Info'
 
 $Total_XML_Files = ( Get-ChildItem XML-Network-Info | Measure-Object ).Count;
 
-Write-Host "$Total_XML_Files network XML files exported to the desktop."
+Write-Host "$Total_XML_Files network XML files exported to the 'XML-Network-Info' directory."
+
+Sleep 5
+
+Introduction
 
 }
 

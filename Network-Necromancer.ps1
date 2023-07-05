f88f08b0fd8e29142f8d17729efe71d7dfdee95e
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-07-05 18:23:14 -0400

    Update Network-Necromancer.ps1

--- Network-Necromancer.ps1
+++ Network-Necromancer.ps1
@@ -58,13 +58,17 @@ $Network_Count = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=
 %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} |
 %{[PSCustomObject]@{ Network = $name;Password = $pass }} | Measure-Object | Select-Object -expandproperty Count | Out-String
 
-Write-Host "You currently have '$Network_Count' networks on your device."
+Write-Host "You currently have $Network_Count networks on your device." -ForegroundColor "Yellow"
 
 Sleep 2 
 
 Introduction
-
+#foodenvy
 }
+
+#netsh wlan delete profile name=LinkTest
+#netsh wlan add profile filename="LinkTest.xml" user=current
+#netsh wlan connect name=Uplink
 
 ####################################################### Third main option ###############################################################
 
@@ -106,7 +110,64 @@ Introduction
 
 }
 
+####################################################### Fourth main option ###############################################################
+
+if($Introduction -eq "4"){
+
+$All_Networks = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | 
+%{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | 
+%{[PSCustomObject]@{ Network = $name;Password = $pass }} | Format-Table -AutoSize | Out-Host
+
+Write-Host $All_Networks -ForegroundColor "Green"
+
+$Network_To_XML = Read-Host "Which network would you like to backup to an XML file?"
+
+if($Network_To_XML -notmatch $All_Networks){
+
+Write-Host "$Network_To_XML is not located on the device, please try again."
+
+Introduction
+
 }
+
+if (Test-Path "XML-Network-Info"){
+
+$Network_Search = Read-Host "$Network_To_XML will be exported to the 'XML-Network-Info' directory in the same directory as this script. Press any key to continue."
+
+netsh wlan export profile name=$Network_To_XML key=clear folder='XML-Network-Info'
+
+$Total_XML_Files = ( Get-ChildItem XML-Network-Info | Measure-Object ).Count;
+
+Write-Host "$Network_To_XML exported to the 'XML-Network-Info' directory."
+
+Sleep 2
+
+Introduction
+
+}
+
+else{
+
+New-Item -Path 'XML-Network-Info' -ItemType Directory
+
+Write-Host "All networks will be exported to the 'XML-Network-Info' directory in the same directory as this script. Press any key to continue."
+
+netsh wlan export profile key=clear folder='XML-Network-Info'
+
+$Total_XML_Files = ( Get-ChildItem XML-Network-Info | Measure-Object ).Count;
+
+Write-Host "$Total_XML_Files network XML files exported to the 'XML-Network-Info' directory."
+
+Sleep 5
+
+Introduction
+
+}
+
+}
+
+}
+
 Introduction 
 
 # All wifi networks and passwords

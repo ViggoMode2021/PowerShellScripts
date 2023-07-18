5abee11ce0448d6895ea16511a810641ed10d825
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-07-18 15:42:35 -0400

    Add files via upload

---
+++ GH.ps1
@@ -0,0 +1,17 @@
+﻿
+<#
+Invoke-WebRequest 'https://github.com/ViggoMode2021/PowerShellScripts/archive/refs/heads/main.zip' -OutFile .\PowerShellScripts.zip
+Expand-Archive .\PowerShellScripts.zip .\
+Rename-Item .\PowerShellScripts-main .\PowerShellScripts
+Remove-Item .\PowerShellScripts.zip
+#>
+
+# https://4sysops.com/archives/powershell-invoke-webrequest-parse-and-scrape-a-web-page/
+
+# https://pipe.how/invoke-webscrape/
+
+# https://blog.ironmansoftware.com/daily-powershell/powershell-download-github/
+
+$URL = 'https://github.com/ViggoMode2021?tab=repositories'
+$Site = Invoke-WebRequest $URL
+$Site.Links | Where-Object innerText -eq 'Login'

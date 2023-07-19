88ce2908ef94556b4555015df99bd275a776df5e
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-07-19 16:54:59 -0400

    Update GH.ps1

--- GH.ps1
+++ GH.ps1
@@ -1,4 +1,3 @@
-﻿
 <#
 Invoke-WebRequest 'https://github.com/ViggoMode2021/PowerShellScripts/archive/refs/heads/main.zip' -OutFile .\PowerShellScripts.zip
 Expand-Archive .\PowerShellScripts.zip .\
@@ -12,6 +11,28 @@ Remove-Item .\PowerShellScripts.zip
 
 # https://blog.ironmansoftware.com/daily-powershell/powershell-download-github/
 
-$URL = 'https://github.com/ViggoMode2021?tab=repositories'
+$GitHub_Profile_Name = Read-Host "What is the name of the GitHub profile you would like to use?"
+
+$GitHub_Profile_Name_Length = $GitHub_Profile_Name.Length
+
+$URL_Suffix = $GitHub_Profile_Name + '?tab=repositories'
+
+$URL = "https://github.com/$URL_Suffix"
+
 $Site = Invoke-WebRequest $URL
-$Site.Links | Where-Object innerText -eq 'Login'
+
+$GitHub_Profile_Name_With_Slashes = '/' + $GitHub_Profile_Name + '/'
+
+$Links = $Site.Links | Select href | Out-String
+
+Write-Host $Links -join ""
+
+#$Position = $Yes.IndexOf("/")
+
+#$Before = $Yes.Substring($GitHub_Profile_Name_Length , $Position)
+
+#Write-Host $Before
+
+#$After = $Yes.Substring($Position+1)
+
+
